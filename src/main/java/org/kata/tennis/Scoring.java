1c27adeb1d0ee25536e145885716b55eb6171246
Tamsir Ousmane Sall <salltamsirousmane@gmail.com>
2024-03-27 20:16:42 +0100

    indent code

--- src/main/java/org/kata/tennis/Scoring.java
+++ src/main/java/org/kata/tennis/Scoring.java
@@ -4,31 +4,31 @@ import java.util.Map;
 
 public class Scoring {
     private boolean finished;
-    private static Map<Integer, String> scoreMap = Map.of(0,"0",1,"15",2,"30",3,"40");
+    private static Map<Integer, String> scoreMap = Map.of(0, "0", 1, "15", 2, "30", 3, "40");
     private static final int firstDeucePoint = 3;
 
-    private static final String  SPACE = " ";
+    private static final String SPACE = " ";
     private static final String COLON = " : ";
     private static final String SLASH = " / ";
 
-    /** Get score after a sequence of points
+    /**
+     * Get score after a sequence of points
      *
      * @param score Game points sequence
      * @return String representation of the score after each won ball
      */
-    public String computeSequence(String score){
+    public String computeSequence(String score) {
         checkScoreFormat(score);
         Player playerA = new Player("A");
         Player playerB = new Player("B");
 
         StringBuilder globalScore = new StringBuilder();
-        for(char currentChar : score.toCharArray()){
-            if(!this.finished){
-                if(currentChar == 'A'){
+        for (char currentChar : score.toCharArray()) {
+            if (!this.finished) {
+                if (currentChar == 'A') {
                     playerA.doScore();
                     globalScore.append(computeSnapshotScore(playerA, playerB)).append('\n');
-                }
-                else {
+                } else {
                     playerB.doScore();
                     globalScore.append(computeSnapshotScore(playerA, playerB)).append('\n');
                 }
@@ -37,9 +37,11 @@ public class Scoring {
         }
         return globalScore.toString();
     }
-    /** Method used to get a snapshot score
+
+    /**
+     * Method used to get a snapshot score
      *
-     * @param firstPlayer Player A
+     * @param firstPlayer  Player A
      * @param secondPlayer Player B
      * @return String representation of the current score
      */
@@ -49,7 +51,7 @@ public class Scoring {
 
         //Regular Score => [0-40]
         if (aScore != null && bScore != null) {
-            if(isFirstDeuce(firstPlayer, secondPlayer)){
+            if (isFirstDeuce(firstPlayer, secondPlayer)) {
                 return Status.DEUCE.getValue();
             }
             return firstPlayer + COLON + scoreMap.get(firstPlayer.getPoints()) + SLASH + secondPlayer + COLON + scoreMap.get(secondPlayer.getPoints());
@@ -57,31 +59,35 @@ public class Scoring {
         //A player won
         if (hasWinner(firstPlayer, secondPlayer)) {
             finished = true;
-            return getLeader(firstPlayer, secondPlayer) +SPACE+ Status.WIN.getValue();
+            return getLeader(firstPlayer, secondPlayer) + SPACE + Status.WIN.getValue();
         }
         // A player has AD
         if (isAdvantage(firstPlayer, secondPlayer)) {
-            return Status.AD.getValue()+SPACE+getLeader(firstPlayer, secondPlayer);
+            return Status.AD.getValue() + SPACE + getLeader(firstPlayer, secondPlayer);
         }
         //Else deuce
         return Status.DEUCE.getValue();
 
     }
+
     private boolean hasWinner(Player a, Player b) {
         return Math.abs(a.getPoints() - b.getPoints()) > 1;
     }
+
     private boolean isAdvantage(Player a, Player b) {
         return Math.abs(a.getPoints() - b.getPoints()) == 1;
     }
 
-    private boolean isFirstDeuce(Player a, Player b){
+    private boolean isFirstDeuce(Player a, Player b) {
         return a.getPoints() == firstDeucePoint && b.getPoints() == firstDeucePoint;
     }
+
     private Player getLeader(Player a, Player b) {
         return a.getPoints() > b.getPoints() ? a : b;
     }
-    private void checkScoreFormat (String score){
-        if(score == null || score.isEmpty()){
+
+    private void checkScoreFormat(String score) {
+        if (score == null || score.isEmpty()) {
             throw new IllegalArgumentException("Score must have at least one point");
         }
         if (!score.matches("[AB]+")) {
